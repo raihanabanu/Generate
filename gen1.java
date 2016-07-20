3f69132041a50f8b78ce6a2464f705b4570945fa
raihanabanu <raihanabanu.s@gmail.com>
2016-07-20 12:38:39 +0530

    Create gen1.java

---
+++ gen1.java
@@ -0,0 +1,14 @@
+# Generate
+import java.util.Scanner;
+public class gen1 {
+public static void main(String[] args) {
+	Scanner s=new Scanner(System.in);
+	int m=s.nextInt();
+	for(int k=0;k<=16;k++){
+		int  j=m*k;
+		System.out.println(m+"*"+k+"="+j);
+		}
+		}
+		}
+	
+	
